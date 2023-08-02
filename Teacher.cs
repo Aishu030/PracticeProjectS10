c48cbe83ed533a98ff2db2f04e52539826d1ad2f
Aishu030 <aishuramesh031@gmail.com>
2023-08-02 21:16:57 +0530

    firstcommit

---
+++ Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pp4
+{
+    internal class Teacher
+    {
+        public string Name { get; set; }
+        public string ClassAndSection { get; set; }
+        public List<Subject> SubjectsTaught { get; set; }
+
+        public Teacher()
+        {
+            SubjectsTaught = new List<Subject>();
+        }
+    }
+}
